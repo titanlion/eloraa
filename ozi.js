6ad56687968df8d6b1541a71bf363486f3690e8b
Glitch (noble-spangled-gardenia) <none>
2022-04-09 19:09:39 +0000

    🎠🍦 Checkpoint ./ozi.js:40744397/21

--- ozi.js
+++ ozi.js
@@ -57,7 +57,7 @@ require("./src/handlers/mongoHandler");
 require("./src/handlers/functionHandler")(client);
 
 client
-  .login(settings.token)
+  .login(process.env.token)
   .then(() => console.log("Bot Başarıyla Bağlandı!"))
   .catch(() => console.log("[HATA] Bot Bağlanamadı!"));
 
